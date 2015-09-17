8f638d344442e58433be7fa940db5c6e8e699f8d
Fred Block <info@fredblock.com>
2015-09-17 16:09:10 -0600

    add example2.js.
    new file:   test/example2.js
    modified:   test/index.js

---
+++ test/example2.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('example2.js tests', function () {
+  it('is loaded from index.js', function () {
+    assert(true);
+  });
+});
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
-require('./example1.js')
+require('./example1.js');
+require('./example2.js');
 
 describe('the test bundle', function () {
   it('is wired up', function () {
